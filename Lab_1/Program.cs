a9c1ddf56f35fe5170ff5d620c4b1486007c1d5d
Professional <Professional@USER-PC>
2020-06-04 15:53:04 +0300

    Добавьте файлы проекта.

---
+++ Lab_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp45
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double radius = 3;
+            Console.Write("X: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Y: ");
+            double y = Convert.ToDouble(Console.ReadLine());
+            if (((x * y < 0) || (Math.Abs(y) > Math.Abs(y)) || (Math.Abs(radius - y) * Math.Abs(radius - y) + Math.Abs(radius - x) * Math.Abs(radius - x) > radius * radius)))
+                Console.WriteLine("Не принадлежит.");
+            else
+                Console.WriteLine("Принадлежит.");
+            Console.ReadLine();
+        }
+    }
+}
